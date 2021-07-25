e4963faae404f1e6f8ac781bac7676aae6ba9d4b
seo-K <dkasid@naver.com>
2021-07-25 12:29:07 +0900

    todo 예제 추가

---
+++ src/basic/object.js
@@ -0,0 +1,17 @@
+/*
+객체
+{ key : value }
+데이터 구조화 하기 위한 수단
+*/
+
+const food = {
+  name: "떡뽁이",
+  ingredients: ["대파", "고추장", "떡"],
+}
+
+//상자
+const box = {
+  width: 100,
+  height: 100,
+  backgorndColor: "#efddfd",
+}
